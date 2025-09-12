65ce96c7d3594e139ac4d31a81855087ee255900
Daniyel Vanguard <123575859+Daniyel-Vanguard@users.noreply.github.com>
2025-09-12 12:56:35 -0400

    Create BotIntegrationTest.php

---
+++ tests/Integration/BotIntegrationTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace RubikaBot\Tests\Integration;
+
+use PHPUnit\Framework\TestCase;
+use RubikaBot\Core\Bot;
+
+class BotIntegrationTest extends TestCase
+{
+    public function testSendMessageIntegration()
+    {
+        $this->markTestIncomplete('Integration test requires mock API setup');
+    }
+}
